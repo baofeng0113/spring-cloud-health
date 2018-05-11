49dea72bea1d83165fdf0d628c22b0af2cd30a07
baofeng0113 <baofeng0113@gmail.com>
2018-05-11 23:16:35 +0800

    init

--- health-bashboard/src/main/java/cloud/dispatcher/midware/health/dashboard/Application.java
+++ health-bashboard/src/main/java/cloud/dispatcher/midware/health/dashboard/Application.java
@@ -1,24 +1,18 @@
-package cloud.dispatcher.midware.health.client;
+package cloud.dispatcher.midware.health.dashboard;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.SpringCloudApplication;
 
-@SpringBootApplication
-@EnableEurekaClient
+@SpringCloudApplication
 public class Application {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-    @Autowired private DiscoveryClient discoveryClient;
-
     public static void main(String[] args) {
         long currentTimeMillis = System.currentTimeMillis();
-        SpringApplication.run(Application.class);
+        SpringApplication.run(Application.class, args);
         LOGGER.info("Spring boot application startup completed, cost: {}ms",
                 System.currentTimeMillis() - currentTimeMillis);
     }
