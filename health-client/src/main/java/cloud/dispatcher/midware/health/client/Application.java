5fdb647289e53aafc02e635c599039d34fb25bf3
baofeng0113 <baofeng0113@gmail.com>
2018-05-12 09:46:25 +0800

    init

--- health-client/src/main/java/cloud/dispatcher/midware/health/client/Application.java
+++ health-client/src/main/java/cloud/dispatcher/midware/health/client/Application.java
@@ -3,23 +3,36 @@ package cloud.dispatcher.midware.health.client;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableEurekaClient
-public class Application {
+@EnableDiscoveryClient
+@EnableAsync
+public class Application implements CommandLineRunner {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
-    @Autowired private DiscoveryClient discoveryClient;
+    @Value("${spring.cloud.health.interval.ms}") private int intervalMS;
 
     public static void main(String[] args) {
         long currentTimeMillis = System.currentTimeMillis();
-        SpringApplication.run(Application.class);
+        new SpringApplicationBuilder(Application.class).web(false).build().run(args);
         LOGGER.info("Spring boot application startup completed, cost: {}ms",
                 System.currentTimeMillis() - currentTimeMillis);
     }
+
+    @Override
+    public void run(String... strings) throws Exception {
+        while (true) {
+            componentMetricsInfoMonitor.reportMetricsInfo();
+            Thread.sleep(intervalMS);
+        }
+    }
+
+    @Autowired private ComponentMetricsInfoMonitor componentMetricsInfoMonitor;
 }
