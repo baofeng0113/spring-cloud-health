a5caa9996ad6708a77c17c82e2cd99ab357889d8
baofeng0113 <baofeng0113@gmail.com>
2018-05-12 09:45:13 +0800

    init

---
+++ health-client/src/main/java/cloud/dispatcher/midware/health/client/ComponentInstancesDiscovery.java
@@ -0,0 +1,45 @@
+package cloud.dispatcher.midware.health.client;
+
+import java.util.Collections;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.stereotype.Component;
+
+import com.google.common.collect.Lists;
+
+import cloud.dispatcher.base.framework.utils.LocalizeEnvUtil;
+
+@Component
+public class ComponentInstancesDiscovery {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComponentInstancesDiscovery.class);
+
+    @Autowired private DiscoveryClient discoveryClient;
+
+    public List<ServiceInstance> getLocalizeInstanceList() {
+        List<String> serviceIdListOnDiscovery = discoveryClient.getServices();
+        if (serviceIdListOnDiscovery.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        List<ServiceInstance> localizeInstanceList = Lists.newArrayList();
+
+        for (String serviceId : serviceIdListOnDiscovery) {
+            List<ServiceInstance> serviceInstanceList = discoveryClient.getInstances(serviceId);
+            if (!serviceInstanceList.isEmpty()) {
+                serviceInstanceList.forEach(serviceInstance -> {
+                    if (serviceInstance.getHost().equals(LocalizeEnvUtil.getMachineIpv4Address())) {
+                        localizeInstanceList.add(serviceInstance);
+                    }
+                });
+            }
+        }
+
+        return localizeInstanceList;
+    }
+}
