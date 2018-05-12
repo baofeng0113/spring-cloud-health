6ee510fb55c04cb0e8619d3e90e4fccec0aacd30
baofeng0113 <baofeng0113@gmail.com>
2018-05-12 09:45:34 +0800

    init

---
+++ health-client/src/main/java/cloud/dispatcher/midware/health/client/ComponentMetricsInfoMonitor.java
@@ -0,0 +1,39 @@
+package cloud.dispatcher.midware.health.client;
+
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
+
+@Component
+public class ComponentMetricsInfoMonitor {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComponentMetricsInfoMonitor.class);
+
+    @Autowired private ComponentInstancesDiscovery componentInstancesDiscovery;
+
+    @Autowired private ComponentMetricsInfoCapture componentMetricsInfoCapture;
+
+    @Autowired private ComponentMetricsInfoPublish componentMetricsInfoPublish;
+
+    @Async
+    public void reportMetricsInfo() {
+        List<ServiceInstance> instanceList = componentInstancesDiscovery.getLocalizeInstanceList();
+        if (instanceList.isEmpty()) {
+            LOGGER.warn("No running instance on the local");
+        } else {
+            instanceList.forEach(item -> {
+                Map<String, Object> metrics = componentMetricsInfoCapture.captureMetricsInfo(item);
+                if (!CollectionUtils.isEmpty(metrics)) {
+                    componentMetricsInfoPublish.publisMetricsInfo(metrics);
+                }
+            });
+        }
+    }
+}
