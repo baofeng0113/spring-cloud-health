c83ffe53a49be0f7747d0861ce52d4fb1d2d7da2
baofeng0113 <baofeng0113@gmail.com>
2018-05-14 21:31:15 +0800

    init

---
+++ health-dashboard/src/main/java/cloud/dispatcher/midware/health/dashboard/ComponentMetricsDataQuery.java
@@ -0,0 +1,11 @@
+package cloud.dispatcher.midware.health.dashboard;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ComponentMetricsDataQuery {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ComponentMetricsDataQuery.class);
+}
