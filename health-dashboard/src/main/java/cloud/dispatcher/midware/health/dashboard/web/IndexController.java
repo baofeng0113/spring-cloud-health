96c91a2f57038e56246549612c94c5a533934bdb
baofeng0113 <baofeng0113@gmail.com>
2018-05-15 22:46:17 +0800

    init

--- health-dashboard/src/main/java/cloud/dispatcher/midware/health/dashboard/web/IndexController.java
+++ health-dashboard/src/main/java/cloud/dispatcher/midware/health/dashboard/web/IndexController.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import cloud.dispatcher.midware.health.dashboard.ComponentMetricsDiscovery;
@@ -20,12 +21,14 @@ public class IndexController {
     @Autowired private ComponentMetricsDiscovery componentMetricsDiscovery;
 
     @RequestMapping(value = {"/", "/index", "/index.action", "/index.html"})
-    public ModelAndView indexAction(ModelAndView modelAndView) {
+    public ModelAndView indexAction(ModelAndView modelAndView, @RequestParam("service") String selected) {
         List<String> serviceIdList = componentMetricsDiscovery.getServiceIdList();
-        modelAndView.addObject("services", serviceIdList);
-        modelAndView.addObject("selected", serviceIdList.isEmpty() ?
-                StringUtils.EMPTY : serviceIdList.get(0));
-        modelAndView.setViewName("index");
+        if (StringUtils.isBlank(selected) || !serviceIdList.contains(selected)) {
+            modelAndView.addObject("selected", serviceIdList.isEmpty() ? "" : serviceIdList.get(0));
+        } else {
+            modelAndView.addObject("selected", selected);
+        }
+        modelAndView.addObject("services", serviceIdList).setViewName("index");
         return modelAndView;
     }
 }
