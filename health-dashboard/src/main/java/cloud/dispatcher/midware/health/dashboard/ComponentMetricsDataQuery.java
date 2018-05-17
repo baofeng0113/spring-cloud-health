887ef5ad65de469dfed93f4918caedcb768e3b1a
baofeng0113 <baofeng0113@gmail.com>
2018-05-17 18:50:53 +0800

    init

--- health-dashboard/src/main/java/cloud/dispatcher/midware/health/dashboard/ComponentMetricsDataQuery.java
+++ health-dashboard/src/main/java/cloud/dispatcher/midware/health/dashboard/ComponentMetricsDataQuery.java
@@ -1,6 +1,5 @@
 package cloud.dispatcher.midware.health.dashboard;
 
-import java.util.Collections;
 import java.util.List;
 
 import org.slf4j.Logger;
@@ -11,12 +10,9 @@ import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Component;
-import org.springframework.util.CollectionUtils;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.common.collect.Lists;
 
-import cloud.dispatcher.base.framework.utils.CodecUtil;
 import cloud.dispatcher.midware.health.dashboard.config.GlobalConfigValue;
 
 @Component
@@ -39,12 +35,6 @@ public class ComponentMetricsDataQuery {
         Query query = new Query(criteria).with(new Sort(Sort.Direction.ASC,
                 GlobalConfigValue.METRICS_CREATE_TIME));
 
-        List<String> jsonList = mongoTemplate.find(query, String.class, Application.getCollectionName(name));
-        if (CollectionUtils.isEmpty(jsonList)) { return Collections.emptyList(); }
-
-        List<JsonNode> result = Lists.newArrayList();
-        jsonList.forEach(json -> result.add(CodecUtil.JSON.readTree(json)));
-
-        return result;
+        return mongoTemplate.find(query, JsonNode.class, Application.getCollectionName(name));
     }
 }
