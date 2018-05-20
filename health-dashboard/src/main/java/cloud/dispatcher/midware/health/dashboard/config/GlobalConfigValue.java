756a13c36ccaad4fac83073fc4a8a862f1028051
baofeng0113 <baofeng0113@gmail.com>
2018-05-20 18:06:21 +0800

    init

--- health-dashboard/src/main/java/cloud/dispatcher/midware/health/dashboard/config/GlobalConfigValue.java
+++ health-dashboard/src/main/java/cloud/dispatcher/midware/health/dashboard/config/GlobalConfigValue.java
@@ -6,19 +6,19 @@ public final class GlobalConfigValue {
 
     public static final String REST_CONTROLLER_BASE_PACKAGE = "cloud.dispatcher.midware.health.dashboard.web.api";
 
-    public static final String METRICS_APPLICATION_NAME = "application.name";
+    public static final String METRICS_APP_NAME = "application.name";
 
-    public static final String METRICS_APPLICATION_NAME_CONVERTED = "application_name";
+    public static final String METRICS_APP_HOST = "application.host";
 
-    public static final String METRICS_APPLICATION_HOST = "application.host";
-
-    public static final String METRICS_APPLICATION_HOST_CONVERTED = "application_host";
-
-    public static final String METRICS_APPLICATION_PORT = "application.port";
-
-    public static final String METRICS_APPLICATION_PORT_CONVERTED = "application_port";
-
-    public static final String METRICS_CREATE_TIME = "time";
+    public static final String METRICS_APP_PORT = "application.port";
 
     public static final String MONGO_COLLECTION_NAME_PREFIX = "spring_cloud_health_";
+
+    public static final String METRICS_APP_NAME_CONVERTED = "application_name";
+
+    public static final String METRICS_APP_HOST_CONVERTED = "application_host";
+
+    public static final String METRICS_APP_PORT_CONVERTED = "application_port";
+
+    public static final String METRICS_CREATE_TIME = "time";
 }
